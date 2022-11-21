324274c24e2fef031d374a829b37293d5ac05204
ley1233 <113224183+ley1233@users.noreply.github.com>
2022-11-22 00:57:44 +0900

    wetube2

--- src/controllers/videoControll.js
+++ src/controllers/videoControll.js
@@ -1,10 +1,42 @@
+let videos = [
+  {
+    title: "First Video",
+    rating:5,
+    comment:2,
+    createdAt:"2 minutes ago",
+    views:59,
+    id:1
+  },
+  {
+    title: "Second Video",
+    rating:5,
+    comment:2,
+    createdAt:"2 minutes ago",
+    views:59,
+    id:2
+  },
+  {
+    title: "Third Video",
+    rating:5,
+    comment:2,
+    createdAt:"2 minutes ago",
+    views:59,
+    id:3
+  }
+]
+
+
+
 export const trending = (req,res) =>{
-  res.render("home", {pageTitle: "Comes from your controller.", potato:"you"})
+
+  return res.render("home", {pageTitle: "Home", videos})
 }
 
 export const see = (req,res) =>{
-  res.render("watch")
-}
+  const id = req.params.id
+  console.log("Show video", id)
+  return res.render("watch")
+} 
 
 export const edit = (req,res)=>{
   res.render("edit")
